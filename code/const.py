bcdd9ea7365ebbe691eb75d7aed39cc7185906cd
Rafael Riba <rafariba12@gmail.com>
2025-08-17 11:04:43 -0300

    Creating mediator pattern Enemys destuction

--- code/const.py
+++ code/const.py
@@ -22,6 +22,20 @@ ENTITY_SPEED = {
 
 }
 
+ENTITY_HEALTH = {
+    'Level1Bg0': 999,
+    'Level1Bg1': 999,
+    'Level1Bg2': 999,
+    'Level1Bg3': 999,
+    'Level1Bg4': 999,
+    'Level1Bg5': 999,
+    'Level1Bg6': 999,
+    'Player1': 300,
+    'Player2': 300,
+    'Enemy1': 50,
+    'Enemy2': 60,
+}
+
 # M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
@@ -40,7 +54,7 @@ PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
                     'Player2': pygame.K_d}
 PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
                     'Player2': pygame.K_LCTRL}
-#S
+# S
 SPAWN_TIME = 4000
 
 # W
